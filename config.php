a4b961f80bd52fd2db81dd5f7cc715dc67206a4d
Ahdaaa <afilzag@gmail.com>
2023-12-12 23:38:41 +0700

    feat: add bioskop admin

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     $server = "localhost"; 
-    $user = "pmauser";
-    $password = "rafisutrisno";
+    $user = "root";
+    $password = "Anaana123";
     $nama_database = "movie_theatre";
 
     $db = mysqli_connect($server, $user, $password, $nama_database);
